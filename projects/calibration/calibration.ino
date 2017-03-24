eb912a073f291ea81b8695ab085de165619e8337
houqhlab <fz20100109@163.com>
2017-03-24 17:02:22 +0800

    first update on calibration

--- projects/calibration/calibration.ino
+++ projects/calibration/calibration.ino
@@ -13,12 +13,11 @@ void setup() {
 	// initial 7Bot Arm to check the offset values are work
 	Arm.initialMove();
 	delay(500);
-	for (int i = 0; i > -40; i -= 2)
-	{
-		Arm.moveIK5(PVector(0, 207.5, i), PVector(0, 0, -1));
-		delay(1000);
-	}
-	//Arm.moveIK5(PVector(0, 207.5, 49), PVector(0, 0, -1));
+	Arm.moveIK5(PVector(0, 207.5, -29), PVector(0, 0, -1));
+	delay(500);
+	Arm.initialMove();
+	Arm.forcelessMode();
+
 }
 
 void loop() {
