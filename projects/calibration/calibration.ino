cd6d9f6d5957061e171ce3ba0c7e2fbde10eb25b
houqhlab <fz20100109@163.com>
2017-03-28 16:59:22 +0800

    finish calibration & update libraries

--- projects/calibration/calibration.ino
+++ projects/calibration/calibration.ino
@@ -1,25 +1,19 @@
 #include <Arm7Bot.h>
 Arm7Bot Arm;
 
-//double offsetValues[SERVO_NUM] = {20, 0, 0, 0, 0, 0, -50};
-
 void setup() {
-
-//	// Set offset values
-//	for(int i=0; i<SERVO_NUM; i++)
-//	  Arm.offset[i] = offsetValues[i];
-
-	/* Test */
-	// initial 7Bot Arm to check the offset values are work
+	ARMPORT.begin(115200, SERIAL_8N1);
 	Arm.initialMove();
 	delay(500);
+	// (0, 207.5, -29) is the preset position
 	Arm.moveIK5(PVector(0, 207.5, -29), PVector(0, 0, -1));
-	delay(500);
 	Arm.initialMove();
 	Arm.forcelessMode();
-
 }
 
 void loop() {
-
+	// move 7Bot to the preset position
+	// input 254(0xFE), 253(0xFD), 1 in the serial monitor in order
+	// suggest repeating several times
+	Arm.receiveCom();
 }
